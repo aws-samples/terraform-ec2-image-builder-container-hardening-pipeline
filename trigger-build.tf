028ad7effe02850fbb1cd9596f79cb6bed59fed5
Mike Saintcross <msaintcr+lab-commonservices@amazon.com>
2023-01-10 16:59:13 -0500

    modified role

--- trigger-build.tf
+++ trigger-build.tf
@@ -7,47 +7,6 @@ resource "aws_sqs_queue" "container_build_queue" {
   receive_wait_time_seconds = 10
 }
 
-resource "aws_iam_role" "trigger_role" {
-  name               = "${var.ec2_iam_role_name}-eb-trigger-role"
-  assume_role_policy = <<DOC
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "",
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "events.amazonaws.com"
-      },
-      "Action": ["sts:AssumeRole", "sqs:SendMessage"]
-    }
-  ]
-}
-DOC
-}
-
-resource "aws_cloudwatch_event_rule" "inspector_finding" {
-  name        = "hardening-image-build"
-  description = "Trigger hardening Image Re-Build on High or Critical Severity Findings"
-
-  event_pattern = <<EOF
-{
-  "source": ["aws.inspector2"],
-  "detail-type": ["Inspector2 Finding"],
-  "detail": {
-    "severity": [{ "anything-but": [ "LOW", "MEDIUM" ] }]
-  }
-}
-EOF
-}
-
-resource "aws_cloudwatch_event_target" "container_pipeline" {
-  rule      = aws_cloudwatch_event_rule.inspector_finding.name
-  target_id = "StartContainerBuild"
-  arn       = aws_imagebuilder_image_pipeline.this.arn
-  role_arn  = aws_iam_role.trigger_role.arn
-}
-
 resource "aws_cloudwatch_event_rule" "new_image_push" {
   name        = "new-hardened-container-build-push"
   description = "New hardened container image successful push event rule."
@@ -69,5 +28,64 @@ resource "aws_cloudwatch_event_target" "sqs_queue" {
   rule      = aws_cloudwatch_event_rule.new_image_push.name
   target_id = "NewContainerBuild"
   arn       = aws_sqs_queue.container_build_queue.arn
+  role_arn  = aws_iam_role.trigger_role.arn
+}
+
+resource "aws_iam_role" "trigger_role" {
+  name               = "${var.ec2_iam_role_name}-eb-trigger-role"
+  assume_role_policy = <<DOC
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "events.amazonaws.com"
+      },
+      "Action": ["sts:AssumeRole"]
+    }
+  ]
+}
+DOC
+inline_policy {
+    name = "hardening_instance_inline_policy"
+    policy = jsonencode({
+      "Version": "2012-10-17",
+      "Statement": [
+      {
+          "Sid": "EventsToSQS",
+          "Effect": "Allow",
+          "Principal": {
+            "Service": "events.amazonaws.com"
+          },
+          "Action": "sqs:SendMessage",
+          "Resource": "arn:aws:sqs:${var.aws_region}:${var.account_id}:${aws_sqs_queue.container_build_queue.name}"
+          }
+      ]
+    }
+    )
+}
+}
+
+resource "aws_cloudwatch_event_rule" "inspector_finding" {
+  name        = "hardening-image-build"
+  description = "Trigger hardening Image Re-Build on High or Critical Severity Findings"
+
+  event_pattern = <<EOF
+{
+  "source": ["aws.inspector2"],
+  "detail-type": ["Inspector2 Finding"],
+  "detail": {
+    "severity": [{ "anything-but": [ "LOW", "MEDIUM" ] }]
+  }
+}
+EOF
+}
+
+resource "aws_cloudwatch_event_target" "container_pipeline" {
+  rule      = aws_cloudwatch_event_rule.inspector_finding.name
+  target_id = "StartContainerBuild"
+  arn       = aws_imagebuilder_image_pipeline.this.arn
   role_arn  = aws_iam_role.trigger_role.arn
 }
